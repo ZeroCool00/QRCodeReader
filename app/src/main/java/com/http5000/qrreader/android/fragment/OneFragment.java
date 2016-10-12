33d98ce8259b0d723c93f34644933f9c9db973c0
jaysukh <jaysukh.onus@gmail.com>
2016-10-12 15:36:34 +0530

    All Done

--- app/src/main/java/com/http5000/qrreader/android/fragment/OneFragment.java
+++ app/src/main/java/com/http5000/qrreader/android/fragment/OneFragment.java
@@ -4,10 +4,15 @@ import android.app.SearchManager;
 import android.content.Intent;
 import android.graphics.PointF;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.ShareCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
@@ -22,17 +27,19 @@ import com.http5000.qrreader.android.helper.PointsOverlayView;
 import com.http5000.qrreader.android.helper.RealmController;
 
 import java.util.ArrayList;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 import io.realm.Realm;
 
 
-public class OneFragment extends android.support.v4.app.DialogFragment implements QRCodeReaderView.OnQRCodeReadListener {
+public class OneFragment extends android.support.v4.app.DialogFragment implements QRCodeReaderView.OnQRCodeReadListener, View.OnClickListener {
     private QRCodeReaderView qrCodeReaderView;
     private TextView resultTextView;
     private PointsOverlayView pointsOverlayView;
     private CheckBox flashlightCheckBox;
     private Realm realm;
-
+    FloatingActionButton backButton;
     private AppCompatButton btnSearch, btnShare;
     Model model;
     ArrayList<Model> models = new ArrayList<Model>();
@@ -54,6 +61,7 @@ public class OneFragment extends android.support.v4.app.DialogFragment implement
         View v = inflater.inflate(R.layout.fragment_camera, container, false);
         v.startAnimation(AnimationUtils.loadAnimation(getActivity(), android.R.anim.fade_in));
 
+
         this.realm = RealmController.with(getActivity()).getRealm();
 
         qrCodeReaderView = (QRCodeReaderView) v.findViewById(R.id.qrdecoderview);
@@ -62,6 +70,8 @@ public class OneFragment extends android.support.v4.app.DialogFragment implement
         flashlightCheckBox = (CheckBox) v.findViewById(R.id.flashlight_checkbox);
         btnSearch = (AppCompatButton) v.findViewById(R.id.btnSearchWeb);
         btnShare = (AppCompatButton) v.findViewById(R.id.btnShareLink);
+        backButton = (FloatingActionButton) v.findViewById(R.id.back_button);
+        backButton.setOnClickListener(this);
 
         flashlightCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -80,13 +90,15 @@ public class OneFragment extends android.support.v4.app.DialogFragment implement
         return v;
     }
 
+
     @Override
     public void onQRCodeRead(final String text, PointF[] points) {
         resultTextView.setText(text);
         pointsOverlayView.setPoints(points);
 
-        if (text.length() > 0) {
+        if (text.length() > 0 && !text.isEmpty()) {
             qrCodeReaderView.stopCamera();
+            resultTextView.setVisibility(View.VISIBLE);
             btnSearch.setVisibility(View.VISIBLE);
             btnShare.setVisibility(View.VISIBLE);
 
@@ -113,19 +125,15 @@ public class OneFragment extends android.support.v4.app.DialogFragment implement
             });
 
             model = new Model();
-            model.setQrText(text);
-            models.add(model);
-
-            for(Model b : models){
-                realm.beginTransaction();
-                realm.copyToRealm(model);
-                realm.commitTransaction();
-            }
-
-
+            model.setQrText(resultTextView.getText().toString());
+            realm.beginTransaction();
+            realm.copyToRealm(model);
+            realm.commitTransaction();
         }
 
     }
+
+
 
     @Override
     public void onResume() {
@@ -141,6 +149,14 @@ public class OneFragment extends android.support.v4.app.DialogFragment implement
         super.onPause();
         if (qrCodeReaderView != null) {
             qrCodeReaderView.stopCamera();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+         if (id == R.id.back_button) {
+           getDialog().dismiss();
         }
     }
 }
