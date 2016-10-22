6ab958eb2417685fa1ce357613544e57c54fbcb2
jaysukh <jaysukh.onus@gmail.com>
2016-10-22 12:23:45 +0530

    vcard implementation

--- app/src/main/java/com/http5000/qrreader/android/fragment/OneFragment.java
+++ app/src/main/java/com/http5000/qrreader/android/fragment/OneFragment.java
@@ -1,38 +1,40 @@
 package com.http5000.qrreader.android.fragment;
 
+import android.Manifest;
 import android.app.SearchManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.PointF;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.ShareCompat;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.AppCompatButton;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
-import com.google.zxing.client.result.VCardResultParser;
 import com.http5000.qrreader.android.R;
 import com.http5000.qrreader.android.helper.Model;
 import com.http5000.qrreader.android.helper.PointsOverlayView;
 import com.http5000.qrreader.android.helper.RealmController;
 
-import net.glxn.qrgen.core.scheme.VCard;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
+import java.util.List;
 
+import ezvcard.Ezvcard;
+import ezvcard.VCard;
 import io.realm.Realm;
 
 
@@ -46,6 +48,8 @@ public class OneFragment extends android.support.v4.app.DialogFragment implement
     private AppCompatButton btnSearch, btnShare;
     Model model;
     ArrayList<Model> models = new ArrayList<Model>();
+    private static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
+
 
     public static OneFragment newInstance() {
         OneFragment fragmentFullScreen = new OneFragment();
@@ -64,7 +68,7 @@ public class OneFragment extends android.support.v4.app.DialogFragment implement
         View v = inflater.inflate(R.layout.fragment_camera, container, false);
         v.startAnimation(AnimationUtils.loadAnimation(getActivity(), android.R.anim.fade_in));
 
-
+        File vcfFile = new File(getActivity().getExternalFilesDir(null), "generated.vcf");
         this.realm = RealmController.with(getActivity()).getRealm();
 
         qrCodeReaderView = (QRCodeReaderView) v.findViewById(R.id.qrdecoderview);
@@ -99,6 +103,7 @@ public class OneFragment extends android.support.v4.app.DialogFragment implement
         resultTextView.setText(text);
         pointsOverlayView.setPoints(points);
 
+
         if (text.length() > 0 && !text.isEmpty()) {
             qrCodeReaderView.stopCamera();
             resultTextView.setVisibility(View.VISIBLE);
@@ -118,7 +123,25 @@ public class OneFragment extends android.support.v4.app.DialogFragment implement
             btnShare.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    VCard vCard = new VCard(text);
+
+                    if(checkandRequestPermission()){
+
+                        Toast.makeText(getActivity(), "Working on it", Toast.LENGTH_LONG).show();
+                        /*VCard vCard = Ezvcard.parse(text).first();
+
+                        File vcfFile = new File(getActivity().getExternalFilesDir(null), "generated.vcf");
+
+                        try {
+                            vCard.write(vcfFile);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        Intent i = new Intent();
+                        i.setAction(android.content.Intent.ACTION_VIEW);
+                        i.setDataAndType(Uri.fromFile(vcfFile), "text/x-vcard");
+                        startActivity(i);*/
+                    }
                 }
             });
 
@@ -129,6 +152,39 @@ public class OneFragment extends android.support.v4.app.DialogFragment implement
             realm.commitTransaction();
         }
 
+    }
+
+    private boolean checkandRequestPermission() {
+
+        int readphoestate = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_PHONE_STATE);
+        int storageread = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE);
+        int storagewrite = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int readcontact = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_CONTACTS);
+        int writecontact = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_CONTACTS);
+
+
+        List<String> listpermissionNeeded = new ArrayList<>();
+
+        if (readphoestate != PackageManager.PERMISSION_GRANTED) {
+            listpermissionNeeded.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if (storageread != PackageManager.PERMISSION_GRANTED) {
+            listpermissionNeeded.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+        if (storagewrite != PackageManager.PERMISSION_GRANTED) {
+            listpermissionNeeded.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (readcontact != PackageManager.PERMISSION_GRANTED) {
+            listpermissionNeeded.add(Manifest.permission.READ_CONTACTS);
+        }
+        if (writecontact != PackageManager.PERMISSION_GRANTED) {
+            listpermissionNeeded.add(Manifest.permission.WRITE_CONTACTS);
+        }
+        if (!listpermissionNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(getActivity(), listpermissionNeeded.toArray(new String[listpermissionNeeded.size()]), REQUEST_ID_MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
     }
 
 
